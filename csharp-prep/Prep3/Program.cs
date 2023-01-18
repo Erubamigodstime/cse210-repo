175183aa458f572f65d62ecc39f5e8fb93a28643
Erubamigodstime <erubamigodstime43@gmail.com>
2023-01-18 13:42:20 +0100

    prep3 edited

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string play = "yes";
+        while(play == "yes")
+        {       
+            int number = 300;       
+            int attempt = 0;
+            int guess = 0;
+            Random randomGenerator = new Random();
+            number = randomGenerator.Next(1, 10);        
+            while (guess != number)
+            {               
+                Console.Write("What is the magic Number?: ");
+                string input = Console.ReadLine();
+                guess = int.Parse(input);
+                
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if ( guess == number)
+                {
+                    Console.WriteLine("Hurray You guessed it right");
+                }
+                attempt ++;              
+                
+            }            
+            Console.WriteLine($" It took you {attempt}, attempts to guess it right");
+            Console.Write("Do you want to keep playing?: ");
+            play = Console.ReadLine();
+        
+    }
+
+
+    
+
+
     }
 }
