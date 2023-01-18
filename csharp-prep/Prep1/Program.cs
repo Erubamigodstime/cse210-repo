cf6a04e50426262497e67ba4abf099ac4a7bdad1
Erubamigodstime <erubamigodstime43@gmail.com>
2023-01-18 13:42:27 +0100

    prep3 edited

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+      Console.Write("what is your first Name?");
+      string first = Console.ReadLine();
+      Console.Write("what is your Last Name?");
+      string Last = Console.ReadLine();
+      Console.WriteLine($" My Name is {first}, {Last} {first} ");
     }
 }
