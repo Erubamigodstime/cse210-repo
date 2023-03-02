4f7b5377673f33902789acb039043e355c3c8929
Erubamigodstime <erubamigodstime43@gmail.com>
2023-03-02 09:27:44 +0100

    Learning02 assingment done

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
-    }
+        Job job1 = new Job();
+        job1._jobTitle = "Facilty Manager";
+        job1._company = " Max-Migold";
+        job1._startYear = 2000;
+        job1._endYear = 2022;
+        // job1.DisplayJobDetails();
+        Job job2 = new Job();
+        job2._jobTitle = "Software devlopment";
+        job2._company = "Tesbo Rapids";
+        job2._startYear = 2017;
+        job2._endYear = 2001;
+        // job2.DisplayJobDetails();
+
+        Resume resume1 = new Resume();
+        resume1._personName = "Godstime";
+        resume1._job.Add(job1);
+        resume1._job.Add(job2);
+        resume1.DisplayResumeDetails();
+
+    }                                                                                                                                                                          
+    
 }
